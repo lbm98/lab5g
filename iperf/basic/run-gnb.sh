9573627a506bb48a04ff24d78cfa7cf103c5a06c
lbm <lars.moons@student.uantwerpen.be>
2023-06-13 17:07:25 +0200

    part 3

---
+++ iperf/basic/run-gnb.sh
@@ -0,0 +1,1 @@
+RFSIMULATOR=server ./nr-softmodem -O gnb.sa.band78.106prb.rfsim.conf --parallel-config PARALLEL_SINGLE_THREAD --rfsim --phy-test
---
+++ iperf/basic/run-ue.sh
@@ -0,0 +1,1 @@
+RFSIMULATOR=127.0.0.1 ./nr-uesoftmodem --rfsim --phy-test --rrc_config_path .
